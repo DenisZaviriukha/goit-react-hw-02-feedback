5a9b3645f6bbd7f261b8c8b68f6166e05c20cbad
DenisZaviriukha <dv.zavirukha@gmail.com>
2023-05-14 19:34:35 +0300

    Preparation for work

---
+++ src/components/Layout/Layout.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    margin: 0 auto;
+    gap: 10px;
+    padding: 15px 0;
+    display: flex;
+    flex-direction: column;
+`
